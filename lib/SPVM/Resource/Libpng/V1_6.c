1edcf1220acb6a84e24a28aa2a9dcad79987fe1b
Yuki Kimoto <kimoto.yuki@gmail.com>
2021-12-22 16:11:39 +0900

    use libpng structure

--- lib/SPVM/Resource/Libpng/V1_6.c
+++ lib/SPVM/Resource/Libpng/V1_6.c
@@ -4,11 +4,15 @@
 #include <fenv.h>
 #include <complex.h>
 
+#include <png.h>
+
 static const char* MFILE = "SPVM/Math.c";
 
 int32_t SPVM__Resource__Libpng__V1_6__dummy(SPVM_ENV* env, SPVM_VALUE* stack) {
   (void)env;
   
+  png_colorp palette;
+
   stack[0].ival = 1;
   
   return 0;
--- t/basic.t
+++ t/basic.t
@@ -3,8 +3,11 @@ use warnings;
 use FindBin;
 use lib "$FindBin::Bin/lib";
 
+use DynaLoader;
+
 use Test::More 'no_plan';
 
 use SPVM 'Resource::Libpng::V1_6';
+
 
 is(SPVM::Resource::Libpng::V1_6->dummy, 1);
