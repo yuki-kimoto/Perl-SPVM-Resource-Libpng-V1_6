e2f00bd5f4d28fe8b7eb6c000565eddc84000a79
Yuki Kimoto <kimoto.yuki@gmail.com>
2023-04-05 08:33:48 +0900

    The license is changed to MIT License.
      [License Change]
        * The license is changed to MIT License.

--- lib/SPVM/Resource/Libpng.pm
+++ lib/SPVM/Resource/Libpng.pm
@@ -11,3 +11,9 @@ SPVM::Resource::Libpng - libpng porting to Perl/SPVM
 =head1 DESCRIPTION
 
 SPVM::Resource::Libpng is a libpng porting to Perl/SPVM. Current libpng version is 1.6.37.
+
+=head1 Copyright & License
+
+Copyright (c) 2023 Yuki Kimoto
+
+MIT License
